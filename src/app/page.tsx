9a0cea1db869982f3fc3ade204c43131f9a43823
Luis Dergal <luisandresdergal@gmail.com>
2025-01-04 00:21:50 -0600

    using next js image and information about this project :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
-      <img
+      <Image
         src="/archi.jpg"
         alt="Archi"
         width={450} 
@@ -11,6 +11,7 @@ export default function Home() {
         className="rounded-lg shadow-lg"
       />
       <h1 className="text-3xl font-bold mt-4">Control Panel</h1>
+      <h2> I&apos;m Building this site on my free time, so there is no hurry to finish it soon, but I really enjoy to work on this. </h2>
     </main>
   );
 }
