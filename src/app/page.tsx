bdc5861ffe401ba95939ec03535248a4842114ea
Luis Dergal <luisandresdergal@gmail.com>
2025-01-04 00:19:02 -0600

    fixing img uppercase bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
-      <Image
+      <img
         src="/archi.jpg"
         alt="Archi"
         width={450} 
